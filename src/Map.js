a5aed638ad71aff61c3b50a8ffcd4cc24f75d968
Gaurav Talwar <megaurav2002@gmail.com>
2017-06-21 21:04:47 +1000

    added styled-components

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Map = () => <div>Map</div>;
+import Styled from "styled-components";
+
+const MapContainer = Styled.div`    
+    margin: 10px 0 0 0;
+    height: 400px;
+`;
+
+class Map extends React.Component {
+  render() {
+    return <MapContainer>Map</MapContainer>;
+  }
+}
 
 export default Map;
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,10 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 
 const header = () =>
   <div className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <h2>YouPick</h2>
+    <h3>YouPick</h3>
   </div>;
 
 export default header;
